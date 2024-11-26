fc1d3389519836cda14c5fa43f4a0b13976157d3
Guilherme Costanzi Do <guilhermecostanzi@gmail.com>
2024-11-26 14:31:57 -0300

    Adicionando mais exclusões para explicitar o comportamento dinâmico da lista. Melhorias no acompanhamento.

--- ClienteOrdenada.c
+++ ClienteOrdenada.c
@@ -11,7 +11,7 @@ int main() {
 
   // Exibir lista vazia
 
-  printf("\n-- Lista inicial -- \n");
+  printf("\n-- Lista inicial -- \n\n");
 
   exibirLista(&lista);
   printf("Capacidade da lista: %i.\n", capacidade(&lista));
@@ -19,6 +19,9 @@ int main() {
   printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
   // Inserir elementos na lista e mostrar valores e tamanhos
+
+  printf("\n-- Inserindo elementos e acompanhamento da lista --\n");
+
   reg.chave = 9;
   printf("\nAdicionando elemento ... \n");
   inserirElemListaOrd(&lista, reg);
@@ -54,10 +57,32 @@ int main() {
   printf("Capacidade da lista: %i.\n", capacidade(&lista));
   printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
   printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
+  reg.chave = 19;
+  printf("\nAdicionando elemento ... \n");
+  inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Capacidade da lista: %i.\n", capacidade(&lista));
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
+  printf("\n-- Observe o aumento da capacidade a partir desse ponto:  \n");
+  reg.chave = 50;
+  printf("\nAdicionando elemento ... \n");
+  inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Capacidade da lista: %i.\n", capacidade(&lista));
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
+  reg.chave = 52;
+  printf("\nAdicionando elemento ... \n");
+  inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Capacidade da lista: %i.\n", capacidade(&lista));
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
   // Buscar um elemento na lista
 
-  printf("\nBuscando elementos na lista: ");
+  printf("\n-- Buscando elementos na lista -- \n\n ");
 
   int pos = buscaSequencial(&lista, 4);
   printf("Chave 4 encontrada na posicao: %i do arranjo A.\n", pos);
@@ -70,35 +95,26 @@ int main() {
 
   // Excluir alguns elementos da lista
 
-  printf("\n-- Excluindo elementos da lista -- \n");
+  printf("\n-- Excluindo elementos da lista -- \n\n");
 
   if (excluirElemLista(&lista, 4)) printf("Exclusao bem sucedida: 4.\n");
   if (excluirElemLista(&lista, 8)) printf("Exclusao bem sucedida: 8 (não existe na lista).\n");
   if (excluirElemLista(&lista, 9)) printf("Exclusao bem sucedida: 9.\n");
+  if (excluirElemLista(&lista, 19)) printf("Exclusao bem sucedida: 19.\n");
+  if (excluirElemLista(&lista, 50)) printf("Exclusao bem sucedida: 50.\n");
+  if (excluirElemLista(&lista, 52)) printf("Exclusao bem sucedida: 52.\n");
 
   // Exibir lista após exclusões
-  exibirLista(&lista);
-  printf("Capacidade da lista: %i.\n", capacidade(&lista));
-  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
-  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
-  // Inserir elementos em grande quantidade
+  printf("\n-- Novo status da lista depois de excluir os elementos --\n\n");
 
-  printf("\n-- Inserindo elementos em grande quantidade -- \n");
-
-  for(int i = 50; i < 100; i++){
-    reg.chave = i;
-    inserirElemListaOrd(&lista, reg);
-  }
-
-  // Exibir lista após inserts
   exibirLista(&lista);
   printf("Capacidade da lista: %i.\n", capacidade(&lista));
   printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
   printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
   // Reinicializar a lista
-  printf("\n-- Reinicializando a lista --\n");
+  printf("\n-- Reinicializando a lista --\n\n");
   reinicializarLista(&lista);
   exibirLista(&lista);
   printf("Capacidade da lista: %i.\n", capacidade(&lista));
