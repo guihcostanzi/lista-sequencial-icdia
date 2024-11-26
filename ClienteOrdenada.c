7ed5a429e82afb3cac01956009e50a47b1f56c59
Guilherme Costanzi Do <guilhermecostanzi@gmail.com>
2024-11-26 13:41:59 -0300

    Melhoria na visualização dos dados no ClienteOrdenada.c

--- ClienteOrdenada.c
+++ ClienteOrdenada.c
@@ -10,28 +10,49 @@ int main() {
   inicializarLista(&lista);
 
   // Exibir lista vazia
+
+  printf("\n-- Lista inicial -- \n");
+
   exibirLista(&lista);
   printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
   printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
-  // Inserir elementos na lista
+  // Inserir elementos na lista e mostrar valores e tamanhos
   reg.chave = 9;
+  printf("\nAdicionando elemento ... \n");
   inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
   reg.chave = 3;
+  printf("\nAdicionando elemento ... \n");
   inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
   reg.chave = 4;
+  printf("\nAdicionando elemento ... \n");
   inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
   reg.chave = 1;
+  printf("\nAdicionando elemento ... \n");
   inserirElemListaOrd(&lista, reg);
+  exibirLista(&lista);
+  printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
+  printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
   reg.chave = 12;
+  printf("\nAdicionando elemento ... \n");
   inserirElemListaOrd(&lista, reg);
-
-  // Exibir lista após inserções
   exibirLista(&lista);
   printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
   printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
   // Buscar um elemento na lista
+
+  printf("\nBuscando elementos na lista: ");
+
   int pos = buscaSequencial(&lista, 4);
   printf("Chave 4 encontrada na posicao: %i do arranjo A.\n", pos);
   
@@ -42,6 +63,9 @@ int main() {
   printf("Chave 4 encontrada na posicao: %i do arranjo A.\n", pos);
 
   // Excluir alguns elementos da lista
+
+  printf("\n-- Excluíndo elementos da lista -- \n");
+
   if (excluirElemLista(&lista, 4)) printf("Exclusao bem sucedida: 4.\n");
   if (excluirElemLista(&lista, 8)) printf("Exclusao bem sucedida: 8 (não existe na lista).\n");
   if (excluirElemLista(&lista, 9)) printf("Exclusao bem sucedida: 9.\n");
@@ -52,7 +76,7 @@ int main() {
   printf("Tamanho da lista (em bytes): %i.\n", tamanhoEmBytes(&lista));
 
   // Reinicializar a lista
-  printf("-- Reinicializando a lista ... --\n");
+  printf("\n-- Reinicializando a lista --\n");
   reinicializarLista(&lista);
   exibirLista(&lista);
   printf("Numero de elementos na lista: %i.\n", tamanho(&lista));
